aa762b2152fe221d12dc9413a87e7733b221329f
Ashfaq Ahmed <46718979+ashfaq1934@users.noreply.github.com>
2023-03-08 16:27:29 +0000

    used list api view

--- authentication_demo/urls.py
+++ authentication_demo/urls.py
@@ -15,9 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from user.views import api_users
+from user.views import UserList
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', api_users)
+    path('api/', UserList.as_view())
 ]
--- user/views.py
+++ user/views.py
@@ -13,3 +13,10 @@ User = get_user_model()
 class UserList(ListAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+
+    def post(self, request, format=None):
+        serializer = UserSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
