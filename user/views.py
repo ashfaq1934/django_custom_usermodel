f2dbad2fe8c1c381f8980e07ac03ba858e46185e
Ashfaq Ahmed <46718979+ashfaq1934@users.noreply.github.com>
2023-03-08 16:20:38 +0000

    Update views.py

--- user/views.py
+++ user/views.py
@@ -5,16 +5,11 @@ from rest_framework import viewsets
 from user.serializers import UserSerializer
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.generics import ListAPIView
 
 User = get_user_model()
 
-@api_view(('GET',))
-def api_users(request):
-    try:
-        users = User.objects.all()
-    except User.DoesNotExist:
-        return Response(status=status.HTTP_404_NOT_FOUND)
-    if request.method == 'GET':
-        print(users)
-        serializer = UserSerializer(users, many=True)
-        return Response(serializer.data)
+
+class UserList(ListAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
